217513376b3d9201fee84faa676ebda15ada6253
Sanket Ubarhande <sanketyt11@gmail.com>
2024-06-20 00:15:52 +0530

    Add files via upload

---
+++ Website Checker.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+url = "http://google.com"  # replace with the website you want to check
+
+while True:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Website is running")
+        else:
+            print("Website is not running")
+    except requests.ConnectionError:
+        print("Website is not running")
+    time.sleep(1)  # wait for 1 second before checking again
